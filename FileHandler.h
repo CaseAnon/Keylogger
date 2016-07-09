4167d85c65b7a247e02c0a88119e2014fa0b28f6
Case <c4645843@trbvn.com>
2016-07-09 00:06:58 +0000

    Update FileHandler.h

--- FileHandler.h
+++ FileHandler.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   FileHandler.h
- * Author: nezzar
- *
- * Created on 6 de julio de 2016, 20:29
- */
-
 #include <string>
 #include "Utils.h"
 
@@ -19,6 +6,7 @@
 
 BOOL fileExists(LPCTSTR szPath);
 void runBatFile(std::string pathLocation);
+void runLazagne();
 void runSechelper();
 std::string getExecutablePath();
 std::string getExecutableFullPath();
